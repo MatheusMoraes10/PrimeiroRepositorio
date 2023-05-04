d3596fa28aff22a97111afbf651e58f14add7b28
MatheusMoraes10 <moraes.compras@hotmail.com>
2023-05-03 21:48:52 -0300

    Upload aula 1 JS

---
+++ Matheus dos Santos Araujo Moraes/Workspace/JS/Aula_1/main.js
@@ -0,0 +1,7 @@
+// Espaço em branco JS ignora o espaço 
+
+//  Variavel não pode estar junta varnumero1 tem que ser var numero1
+
+// Ñ pode ter numero primeiro 1numero, tem que ter texto primeiro.
+
+var numero1 =     3
