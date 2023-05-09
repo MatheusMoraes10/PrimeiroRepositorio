c0cf8a466727c9d81a419cfb8e1516cad1b6c792
MatheusMoraes10 <moraes.compras@hotmail.com>
2023-05-09 19:26:51 -0300

    Upload exercicio 2  JS

--- Matheus dos Santos Araujo Moraes/Workspace/JS/exercicios/Exercicio_02/main.js
+++ Matheus dos Santos Araujo Moraes/Workspace/JS/exercicios/Exercicio_02/main.js
@@ -2,8 +2,30 @@
 
 var texto1 = "A média do aluno com as 4 notas é: "
 
+let texto2 = "Nota 1: "
 
+let texto3 = "Nota 2: "
 
-var media = 
+let texto4 = "Nota 3: "
+
+let texto5 = "Nota 4: "
+
+let n1 = 7
+
+console.log(texto2 + n1)
+
+let n2 = 8
+
+console.log(texto3 + n2)
+
+let n3 = 10
+
+console.log(texto4 + n3)
+
+let n4 = 9
+
+console.log(texto5 + n4)
+
+var media = (n1 + n2 + n3 +n4)/4
 
 console.log(texto1 + media)
