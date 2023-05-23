80ad04f693d1e3c652311ac7d76746e962627415
MatheusMoraes10 <moraes.compras@hotmail.com>
2023-05-22 21:49:11 -0300

    Upload parte exercicio 6 JS

---
+++ Matheus dos Santos Araujo Moraes/Workspace/3_JS/Exercicios_Entregues/Exercicio_06/scripts/main.js
@@ -0,0 +1,32 @@
+// Exercicio 1
+console.log('Exercicio 1:')
+
+let arr1 = ["HTML", 1993, "CSS", 1996, "Bootstrap", 2011, "JS", 1995, "React", 2013, "Java", 1995]
+
+let arr2 = arr1.toString()
+
+let arr3 = arr1.join(' / ')
+
+let arr4 = arr1.unshift('IOS')
+
+console.log(`Todo Array é: ${arr1}`)
+
+console.log('Tamanho do Array: ' + arr1.length)
+
+console.log('Retornando apenas: ' + arr1[6])
+
+console.log('O tipo da do Array 1 é: ' + typeof arr2)
+
+console.log('Trocando o separador por Barra: ' +  arr3)
+
+console.log(`Adicionando IOS no inicio Aray ficam: ${arr4} posições e a string fica: ` + arr1)
+
+// Exercicio 2
+console.log('Exercicio 2:')
+
+let arrb1 = [
+   ['HTML', 1993, 'CSS', 1996]
+   ['Bootstrap', 2011, 'JS',1995]
+   ['React', 2013,'Java', 1995]
+]
+
