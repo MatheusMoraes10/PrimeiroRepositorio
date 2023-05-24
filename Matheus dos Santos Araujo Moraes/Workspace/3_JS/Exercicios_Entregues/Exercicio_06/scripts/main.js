43f1cc4c403a11198e0eb90d5220593ae1fa2815
MatheusMoraes10 <moraes.compras@hotmail.com>
2023-05-23 21:04:01 -0300

    upload exercicio JS 06

--- Matheus dos Santos Araujo Moraes/Workspace/3_JS/Exercicios_Entregues/Exercicio_06/scripts/main.js
+++ Matheus dos Santos Araujo Moraes/Workspace/3_JS/Exercicios_Entregues/Exercicio_06/scripts/main.js
@@ -7,17 +7,17 @@ let arr2 = arr1.toString()
 
 let arr3 = arr1.join(' / ')
 
-let arr4 = arr1.unshift('IOS')
-
 console.log(`Todo Array é: ${arr1}`)
 
 console.log('Tamanho do Array: ' + arr1.length)
 
-console.log('Retornando apenas: ' + arr1[6])
+console.log('Retornando apenas JS: ' + arr1[6])
 
-console.log('O tipo da do Array 1 é: ' + typeof arr2)
+console.log('O tipo do Array 1 é: ' + typeof arr2)
 
 console.log('Trocando o separador por Barra: ' +  arr3)
+
+let arr4 = arr1.unshift('IOS')
 
 console.log(`Adicionando IOS no inicio Aray ficam: ${arr4} posições e a string fica: ` + arr1)
 
@@ -25,8 +25,31 @@ console.log(`Adicionando IOS no inicio Aray ficam: ${arr4} posições e a string
 console.log('Exercicio 2:')
 
 let arrb1 = [
-   ['HTML', 1993, 'CSS', 1996]
-   ['Bootstrap', 2011, 'JS',1995]
-   ['React', 2013,'Java', 1995]
+   ['HTML', 1993, 'CSS', 1996],
+   ['Bootstrap', 2011, 'JS',1995],
+   ['React', 2013,'Java', 1995],
 ]
+
+console.log(`Todo Array é: ${arrb1}`)
+
+console.log('Tamanho do Array: ' + arrb1.length)
+
+console.log('Retornando apenas: ' + (arrb1[2][1]))
+
+console.log('Substituindo JS por JavaScript (Antes): ' + arrb1[1][2])
+
+arrb1[1][2] = 'JavaScript'
+
+console.log('Substituindo JS por JavaScript (Depois): ' + arrb1[1][2])
+
+console.log('Removendo Java do Array (Antes): ' + arrb1)
+
+delete arrb1[2][2]
+
+console.log('Removendo Java do Array (Depois): ' + arrb1)
+
+let arrb2 = arrb1.push('pipoca')
+
+console.log('inserindo Pipoca final Array: ' + arrb1)
+
 
